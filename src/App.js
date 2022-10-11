81de376d25f476b48ed27356b1d8af8323587dbc
Jaanus Lille <Jaanus.Lille@energia.ee>
2022-10-11 23:00:32 +0300

    Redirecting functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  function getConsoleLog(){
+    // console.log("Täheke");
+    window.location.assign("https://reactjs.org/");
+  }
+
+  useEffect(() => {
+    getConsoleLog()
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
